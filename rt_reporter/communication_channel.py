b7d7c2a7973f5e86344400eec47039d627f684e3
Carlos Gustavo Lopez Pombo <clpombo@dc.uba.ar>
2025-07-07 19:55:34 -0300

    Implementation of the reporter as a RabbitMQ producer.

--- rt_reporter/communication_channel.py
+++ rt_reporter/communication_channel.py
@@ -7,17 +7,20 @@ import threading
 import time
 import struct
 
+import pika
+
 from rt_reporter.communication_channel_conf import CommunicationChannelConf
 
 
 class CommunicationChannel(threading.Thread):
-    def __init__(self, output_file, process_name, timeout):
+    def __init__(self, process_name, host_addr, host_port, timeout):
         super().__init__()
         # Event for controlling the execution of the monitor (TBS by set_event)
         self._stop_event = None
-        # Store files path and process name
+        # Store process name and RabbitMQ server
         self._process_name = process_name
-        self._output_file = output_file
+        self._host_addr = host_addr
+        self._host_port = host_port
         # Reporting time
         self._timeout = timeout
         # Open and clear the main log file
@@ -28,21 +31,31 @@ class CommunicationChannel(threading.Thread):
         self._stop_event = stop_event
 
     def run(self):
-        event_logs_map = {}
         start_time_epoch = time.time()
-        # Create a channel and starts a subprocess.
+        # Create a channel to communicate with the sut and starts a subprocess.
         channel_conf = CommunicationChannelConf()
         channel = subprocess.Popen([self._process_name], stdout=subprocess.PIPE)
-        event_logs_map["main"] = open(self._output_file, "w")
+        # Connection parameters with CLI arguments
+        credentials = pika.PlainCredentials('guest', 'guest')
+        parameters = pika.ConnectionParameters(
+            host=self._host_addr,
+            port=self._host_port,
+            credentials=credentials
+        )
+        # Creat the RabbitMQ connection channel
+        connection = pika.BlockingConnection(parameters)
+        rabbitmq_channel = connection.channel()
+        # Declare exchange for the RabbitMQ connection channel
+        rabbitmq_channel.exchange_declare(exchange='broadcast', exchange_type='fanout')
         while True:
             # Thread control.
             if (self._stop_event.is_set() or
-                    (self._timeout != 0 and time.time()-start_time_epoch >= self._timeout)):
+                    (self._timeout != 0 and time.time() - start_time_epoch >= self._timeout)):
                 break
             buffer = channel.stdout.read(channel_conf.capacity * channel_conf.max_pkg_size)
             # Process packages from communication channel.
             pkgs = [
-                buffer[i : i + channel_conf.max_pkg_size]
+                buffer[i: i + channel_conf.max_pkg_size]
                 for i in range(0, len(buffer), channel_conf.max_pkg_size)
             ]
             for pkg in pkgs:
@@ -55,36 +68,27 @@ class CommunicationChannel(threading.Thread):
                 stripped_data_string = data_string[:1010].strip()
                 match event_type:
                     case 0:
-                        result = (str(timestamp)+","+"timed_event"+","+stripped_data_string)
-                        event_logs_map["main"].write(result + "\n")
+                        result = (str(timestamp) + "," + "timed_event" + "," + stripped_data_string + "\n")
                     case 1:
-                        result = (str(timestamp)+","+"state_event"+","+stripped_data_string)
-                        event_logs_map["main"].write(result + "\n")
+                        result = (str(timestamp) + "," + "state_event" + "," + stripped_data_string + "\n")
                     case 2:
-                        result = (str(timestamp)+","+"process_event"+","+stripped_data_string)
-                        event_logs_map["main"].write(result + "\n")
+                        result = (str(timestamp) + "," + "process_event" + "," + stripped_data_string + "\n")
                     case 3:
-                        result = (str(timestamp)+","+"component_event"+","+stripped_data_string)
-                        event_logs_map["main"].write(result + "\n")
+                        result = (str(timestamp) + "," + "component_event" + "," + stripped_data_string + "\n")
                     case 4:
-                        # "self-loggable component log_init_event"
-                        event_logs_map[stripped_data_string] = open(self._output_file,"w")
-                    case 5:
-                        decoded_data_string = stripped_data_string.split(",", 1)
-                        comp_name = decoded_data_string[0]
-                        result = str(timestamp) + "," + decoded_data_string[1]
-                        event_logs_map[comp_name].write(result + "\n")
-                    case 6:
                         # This case captures the EndOfReportEvent so there is nothing to write.
-                        pass
+                        result = None
                     case _:
-                        event_type_name = "invalid"
-                        result = (str(timestamp)+","+str(event_type_name)+","+stripped_data_string)
-                        event_logs_map["main"].write(result + "\n")
+                        result = (str(timestamp) + "," + "invalid" + "," + stripped_data_string + "\n")
+                if result is not None:
+                    rabbitmq_channel.basic_publish(
+                        exchange='broadcast',
+                        routing_key='',
+                        body=result
+                    )
                 time.sleep(1 / 100000)
         # Close the channel and terminates the subprocess.
         channel.terminate()
         channel.stdout.close()
-        # close the output files
-        for comp_name in event_logs_map:
-            event_logs_map[comp_name].close()
+        # Close the RabbitMQ connection.
+        connection.close()
--- rt_reporter/rt_reporter_sh.py
+++ rt_reporter/rt_reporter_sh.py
@@ -47,25 +47,13 @@ def main():
     parser = argparse.ArgumentParser(
         prog="The Runtime Reporter",
         description="Reports events of a software artifact to be used by The Runtime Monitor.",
-        epilog="Example: python -m rt_reporter.rt_reporter_sh path/to/sut --event_report path/to/output.csv --timeout 5"
+        epilog="Example: python -m rt_reporter.rt_reporter_sh --sut path/to/sut --log path/to/output.csv --host [localhost] --port [5673] --timeout 5"
     )
-    parser.add_argument(
-        "sut",
-        type=str,
-        help="Path to the executable binary."
-    )
-    parser.add_argument(
-        "event_report",
-        type=str,
-        nargs="?",
-        help="Path to the output event report, in csv format."
-    )
-    parser.add_argument(
-        "-t", "--timeout",
-        type=int,
-        nargs="?",
-        help="Timeout for the acquisition process in seconds."
-    )
+    parser.add_argument("sut", type=str, help="Path to the executable binary.")
+    parser.add_argument('--host', default='localhost', help='RabbitMQ server host')
+    parser.add_argument('--port', default=5673, type=int, help='RabbitMQ server port')
+    parser.add_argument("--timeout", type=int, nargs="?", help="Timeout for the acquisition process in seconds.")
+
     # Parse arguments
     args = parser.parse_args()
     input_path = Path(args.sut)
@@ -73,38 +61,14 @@ def main():
     if not valid:
         print(f"Executable binary file error. {message}", file=sys.stderr)
         exit(-1)
-    if args.event_report is not None:
-        output_path = Path(args.event_report)
-        valid, message = validate_output_path(output_path)
-        if not valid:
-            print(f"Event report file error. {message}", file=sys.stderr)
-            exit(-2)
-        else:
-            if output_path.is_file():
-                directory = str(output_path.parent)
-                filename_without_extension = output_path.stem
-                extension = output_path.suffix
-                if extension != ".csv" or extension != "":
-                    extension = ".csv"
-            else:
-                directory = str(output_path.parent)
-                filename_without_extension = input_path.name
-                extension = ".csv"
-    else:
-        directory = str(input_path.parent)
-        filename_without_extension = input_path.name
-        extension = ".csv"
     if args.timeout is None:
         timeout = 0
     else:
         timeout = args.timeout
     sut_file_path = str(input_path)
-    output_file_path = directory+"/"+filename_without_extension+extension
-    print(sut_file_path)
-    print(output_file_path)
     # Creates the thread for the communication channel.
     acquirer = CommunicationChannel(
-        output_file_path, sut_file_path, timeout
+        sut_file_path, args.host_addr, args.host_port, timeout
     )
     # Create a new thread to read from the pipe
     process_thread = threading.Thread(
@@ -128,20 +92,6 @@ def validate_input_path(path):
     # Check if it's a file
     if not path.is_file():
         return False, "Path is not a file."
-    # Check read permission
-    if not os.access(path, os.R_OK):
-        return False, "No read permission."
-    return True, "Path is valid."
-
-
-def validate_output_path(path):
-    try:
-        path.resolve()  # Normalize and validate
-    except (OSError, RuntimeError):
-        return False, "Invalid path syntax or characters."
-    # Check existence
-    if not path.exists():
-        return False, "Path does not exist."
     # Check read permission
     if not os.access(path, os.R_OK):
         return False, "No read permission."
