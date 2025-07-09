5596042bf098672955a813930c14f51cf9a469fb
Carlos Gustavo Lopez Pombo <clpombo@dc.uba.ar>
2025-07-09 06:51:39 -0300

    Final tunning including logging information.

--- rt_reporter/communication_channel.py
+++ rt_reporter/communication_channel.py
@@ -2,30 +2,28 @@
 # Copyright (c) 2024 INVAP, open@invap.com.ar
 # SPDX-License-Identifier: AGPL-3.0-or-later OR Fundacion-Sadosky-Commercial
 
+import logging
 import subprocess
 import threading
 import time
 import struct
-
 import pika
 
 from rt_reporter.communication_channel_conf import CommunicationChannelConf
+from rt_reporter.logging_configuration import LoggingLevel
 
 
 class CommunicationChannel(threading.Thread):
-    def __init__(self, process_name, host_addr, host_port, timeout):
+    def __init__(self, process_name, host, port, timeout):
         super().__init__()
         # Event for controlling the execution of the monitor (TBS by set_event)
         self._stop_event = None
         # Store process name and RabbitMQ server
         self._process_name = process_name
-        self._host_addr = host_addr
-        self._host_port = host_port
+        self._host = host
+        self._port = port
         # Reporting time
         self._timeout = timeout
-        # Open and clear the main log file
-        self._event_logs_map = {}
-        time.sleep(1)
 
     def set_event(self, stop_event):
         self._stop_event = stop_event
@@ -38,15 +36,19 @@ class CommunicationChannel(threading.Thread):
         # Connection parameters with CLI arguments
         credentials = pika.PlainCredentials('guest', 'guest')
         parameters = pika.ConnectionParameters(
-            host=self._host_addr,
-            port=self._host_port,
-            credentials=credentials
+            host=self._host,
+            port=self._port,
+            credentials=credentials,
+            connection_attempts=1,
+            client_properties={'connection_name': 'rt_reporter'}
         )
         # Creat the RabbitMQ connection channel
         connection = pika.BlockingConnection(parameters)
         rabbitmq_channel = connection.channel()
         # Declare exchange for the RabbitMQ connection channel
         rabbitmq_channel.exchange_declare(exchange='broadcast', exchange_type='fanout')
+        # Start the writing process to the RabbitMQ server
+        logging.info(f"Reporting events to RabbitMQ server at {self._host}:{self._port}")
         while True:
             # Thread control.
             if (self._stop_event.is_set() or
@@ -68,24 +70,22 @@ class CommunicationChannel(threading.Thread):
                 stripped_data_string = data_string[:1010].strip()
                 match event_type:
                     case 0:
-                        result = (str(timestamp) + "," + "timed_event" + "," + stripped_data_string + "\n")
+                        event = (str(timestamp) + "," + "timed_event" + "," + stripped_data_string + "\n")
                     case 1:
-                        result = (str(timestamp) + "," + "state_event" + "," + stripped_data_string + "\n")
+                        event = (str(timestamp) + "," + "state_event" + "," + stripped_data_string + "\n")
                     case 2:
-                        result = (str(timestamp) + "," + "process_event" + "," + stripped_data_string + "\n")
+                        event = (str(timestamp) + "," + "process_event" + "," + stripped_data_string + "\n")
                     case 3:
-                        result = (str(timestamp) + "," + "component_event" + "," + stripped_data_string + "\n")
+                        event = (str(timestamp) + "," + "component_event" + "," + stripped_data_string + "\n")
                     case 4:
                         # This case captures the EndOfReportEvent so there is nothing to write.
-                        result = None
+                        event = None
                     case _:
-                        result = (str(timestamp) + "," + "invalid" + "," + stripped_data_string + "\n")
-                if result is not None:
-                    rabbitmq_channel.basic_publish(
-                        exchange='broadcast',
-                        routing_key='',
-                        body=result
-                    )
+                        event = (str(timestamp) + "," + "invalid" + "," + stripped_data_string + "\n")
+                if event is not None:
+                    rabbitmq_channel.basic_publish(exchange='broadcast', routing_key='', body=event)
+                    cleaned_event = event.rstrip('\n\r')
+                    logging.log(LoggingLevel.EVENT, f"Sent event: {cleaned_event}.")
                 time.sleep(1 / 100000)
         # Close the channel and terminates the subprocess.
         channel.terminate()
--- rt_reporter/rt_reporter_sh.py
+++ rt_reporter/rt_reporter_sh.py
@@ -1,14 +1,23 @@
 # Copyright (c) 2024 Fundacion Sadosky, info@fundacionsadosky.org.ar
 # Copyright (c) 2024 INVAP, open@invap.com.ar
 # SPDX-License-Identifier: AGPL-3.0-or-later OR Fundacion-Sadosky-Commercial
+
 import argparse
+import logging
 import os
 import sys
 import threading
 from pathlib import Path
-
+import pika
 from pynput import keyboard
 
+from rt_reporter.logging_configuration import (
+    _set_up_logging,
+    LoggingLevel,
+    LoggingDestination,
+    _configure_logging_destination,
+    _configure_logging_level
+)
 from rt_reporter.communication_channel import CommunicationChannel
 
 # Stop event for finishing the reporting process
@@ -29,73 +38,131 @@ def on_press(key):
 def _run_acquisition(process_thread):
     global stop_event
     # Start the listener in a separate thread
-    listener = keyboard.Listener(on_press=on_press)
-    listener.start()
-    # Configure the monitor by setting up control event.
-    process_thread.set_event(stop_event)
-    # Events setup for managing the running mode.
-    stop_event.clear()
-    # Starts the acquisition thread.
-    process_thread.start()
-    # Waiting for the verification process to finish, either naturally or manually.
-    process_thread.join()
-    listener.stop()
+    with keyboard.Listener(on_press=on_press) as listener:
+        # Configure the monitor by setting up control event.
+        process_thread.set_event(stop_event)
+        # Events setup for managing the running mode.
+        stop_event.clear()
+        # Starts the acquisition thread.
+        process_thread.start()
+        # Waiting for the verification process to finish, either naturally or manually.
+        process_thread.join()
 
 
 def main():
     # Argument processing
     parser = argparse.ArgumentParser(
-        prog="The Runtime Reporter",
+        prog="python -m rt_reporter.rt_reporter_sh",
         description="Reports events of a software artifact to be used by The Runtime Monitor.",
-        epilog="Example: python -m rt_reporter.rt_reporter_sh path/to/sut --host [localhost] --port [5673] --timeout 5"
+        epilog="Example: python -m rt_reporter.rt_reporter_sh path/to/sut --host=[localhost] --port=[5673] --timeout=5"
     )
     parser.add_argument("sut", type=str, help="Path to the executable binary.")
-    parser.add_argument('--host', default='localhost', help='RabbitMQ server host')
-    parser.add_argument('--port', default=5673, type=int, help='RabbitMQ server port')
+    parser.add_argument('--host', default='localhost', required=True, help='RabbitMQ server host')
+    parser.add_argument('--port', default=5673, type=int, required=True, help='RabbitMQ server port')
+    parser.add_argument("--log_level", type=str, choices=["debug", "event", "info", "warnings", "errors", "critical"], default="info", required=False, help="Log verbose level (optional argument)")
+    parser.add_argument('--log_file', required=False, help='Path to log file (optional argument).')
     parser.add_argument("--timeout", type=int, nargs="?", help="Timeout for the acquisition process in seconds.")
 
+    # Declaration of useful functions
+    def validate_input_path(path):
+        try:
+            path.resolve()  # Normalize and validate
+        except (OSError, RuntimeError):
+            return False, "Invalid path syntax or characters."
+        # Check existence
+        if not path.exists():
+            return False, "Path does not exist."
+        # Check if it's a file
+        if not path.is_file():
+            return False, "Path is not a file."
+        # Check read permission
+        if not os.access(path, os.R_OK):
+            return False, "No read permission."
+        return True, "Path is valid."
+
+    def is_rabbitmq(host, port, timeout=2):
+        try:
+            logging.info(f"Testing connection to RabbitMQ server at {args.host}:{args.port}.")
+            credentials = pika.PlainCredentials('guest', 'guest')
+            parameters=pika.ConnectionParameters(
+                host=host,
+                port=port,
+                credentials=credentials,
+                connection_attempts=1,
+                socket_timeout=timeout,
+                stack_timeout=timeout,
+                client_properties={'connection_name': 'rt-reporter.connection_test'}
+            )
+            connection = pika.BlockingConnection(parameters)
+            connection.close()
+            return True
+        except Exception:
+            return False
+
+    # Start the execution of The Runtime Reporter
     # Parse arguments
     args = parser.parse_args()
+    # Set up the logging infrastructure
+    # Configure logging level.
+    if args.log_level is None:
+        logging_level = LoggingLevel.INFO
+    else:
+        match args.log_level:
+            case "debug":
+                logging_level = LoggingLevel.DEBUG
+            case "event":
+                logging_level = LoggingLevel.EVENT
+            case "info":
+                logging_level = LoggingLevel.INFO
+            case "warnings":
+                logging_level = LoggingLevel.WARNING
+            case "errors":
+                logging_level = LoggingLevel.ERROR
+            case "critical":
+                logging_level = LoggingLevel.CRITICAL
+            case _:
+                print(f"Logging level error: {args.log_level} is not a logging level.", file=sys.stderr)
+                exit(-3)
+    # Configure logging destination.
+    if args.log_file is None:
+        logging_destination = LoggingDestination.CONSOLE
+    else:
+        valid, message = validate_input_path(args.log_file)
+        if not valid:
+            print(f"Log file error. {message}", file=sys.stderr)
+            exit(-3)
+        else:
+            logging_destination = LoggingDestination.FILE
+    _set_up_logging()
+    _configure_logging_destination(logging_destination, args.log_file)
+    _configure_logging_level(logging_level)
+    # Validate and normalise the SUT path
     input_path = Path(args.sut)
     valid, message = validate_input_path(input_path)
     if not valid:
         print(f"Executable binary file error. {message}", file=sys.stderr)
         exit(-1)
+    logging.info(f"SUT path: {input_path}")
+    sut_file_path = str(input_path)
+    # Validate the existence of the RabbitMQ server
+    if not is_rabbitmq(args.host, args.port, timeout=2):
+        print(f"RabbitMQ server not active.", file=sys.stderr)
+        exit(-2)
+    logging.info(f"RabbitMQ server active at {args.host}:{args.port}.")
+    # Determine timeout
     if args.timeout is None:
         timeout = 0
     else:
         timeout = args.timeout
-    sut_file_path = str(input_path)
+    logging.info(f"Timeout: {timeout} seconds.")
     # Creates the thread for the communication channel.
-    acquirer = CommunicationChannel(
-        sut_file_path, args.host_addr, args.host_port, timeout
-    )
+    acquirer = CommunicationChannel(sut_file_path, args.host, args.port, timeout)
     # Create a new thread to read from the pipe
-    process_thread = threading.Thread(
-        target=_run_acquisition,
-        args=[acquirer]
-    )
+    process_thread = threading.Thread(target=_run_acquisition, args=[acquirer])
     # Starts the monitor thread
     process_thread.start()
     # Waiting for the verification process to finish, either naturally or manually.
     process_thread.join()
-
-
-def validate_input_path(path):
-    try:
-        path.resolve()  # Normalize and validate
-    except (OSError, RuntimeError):
-        return False, "Invalid path syntax or characters."
-    # Check existence
-    if not path.exists():
-        return False, "Path does not exist."
-    # Check if it's a file
-    if not path.is_file():
-        return False, "Path is not a file."
-    # Check read permission
-    if not os.access(path, os.R_OK):
-        return False, "No read permission."
-    return True, "Path is valid."
 
 
 if __name__ == "__main__":
